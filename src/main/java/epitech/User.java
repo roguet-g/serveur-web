9d8fb75e2b890d1ea320980d6c9e5bba8cb422a1
hirsch-b <benjamin.hirschfield@epitech.eu>
2017-01-12 17:09:52 +0100

    User::getJSON with GSON

--- src/main/java/epitech/User.java
+++ src/main/java/epitech/User.java
@@ -4,30 +4,34 @@ package epitech;
  * Created by senpaisilver on 12/01/2017.
  */
 
+import com.google.gson.Gson;
 import lombok.Data;
 
 import static j2html.TagCreator.*;
 
 public @Data class User {
-    int _id;
-    String _username;
-    String _email;
+    int id;
+    String username;
+    String email;
 
     public User(int id, String username, String email) {
-        this._id = id;
-        this._username = username;
-        this._email = email;
+        this.id = id;
+        this.username = username;
+        this.email = email;
     }
 
     public static Object getHTML() {
         User usr = new User(0, "TotallyGenericUserName", "user@domain");
 
         return body().with(
-                h1(usr._username)
+                h1(usr.username)
         ).render();
     }
 
     public static Object getJSON() {
-        return "{}";
+        User usr = new User(0, "TotallyGenericUserName", "user@domain");
+        Gson gson = new Gson();
+
+        return gson.toJson(usr);
     }
 }
