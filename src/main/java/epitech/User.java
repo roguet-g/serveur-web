3b201e0b067147d38e6f88e687f6f7681a2d420d
hirsch-b <benjamin.hirschfield@epitech.eu>
2017-01-12 16:55:26 +0100

    Add User class

---
+++ src/main/java/epitech/User.java
@@ -0,0 +1,33 @@
+package epitech;
+
+/**
+ * Created by senpaisilver on 12/01/2017.
+ */
+
+import lombok.Data;
+
+import static j2html.TagCreator.*;
+
+public @Data class User {
+    int _id;
+    String _username;
+    String _email;
+
+    public User(int id, String username, String email) {
+        this._id = id;
+        this._username = username;
+        this._email = email;
+    }
+
+    public static Object getHtml() {
+        User usr = new User(0, "TotallyGenericUserName", "user@domain");
+
+        return body().with(
+                h1(usr._username)
+        ).render();
+    }
+
+    public static Object getJSON() {
+        return new User(0, "SomeRandomUser", "user@domain");
+    }
+}
