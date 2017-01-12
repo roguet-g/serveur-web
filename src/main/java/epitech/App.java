df73f1ac67421aaa1e69907a7301338d0e6d10cb
hirsch-b <benjamin.hirschfield@epitech.eu>
2017-01-12 16:02:02 +0100

    Parameters for requests

--- src/main/java/epitech/App.java
+++ src/main/java/epitech/App.java
@@ -17,21 +17,21 @@ public class App extends Jooby {
         delete("/connection", () -> "DELETE Connection");
         /** Feeds **/
         get("/allfeeds", () -> "GET All feeds");
-        get("/feed/:from/:count", () -> "GET Feeds :from index until :from + :count");
-        get("/feed/:id", () -> "GET Feed :id");
+        get("/feed/:from/:count", req -> "GET Feeds "+ req.param("from").intValue() +" index until "+ req.param("from").intValue() +" + "+ req.param("count").intValue());
+        get("/feed/:id", req -> "GET Feed " + req.param("id").intValue());
         post("/feed", () -> "POST new feed");
-        put("/feed/:id", () -> "PUT feed with :id");
-        delete("/feed/:id", () -> "DELETE feed with :id");
+        put("/feed/:id", req -> "PUT feed with " + req.param("id").intValue());
+        delete("/feed/:id", req -> "DELETE feed with " + req.param("id").intValue());
         /** Categories **/
         get("/category", () -> "GET Categories");
-        get("/category/:id", () -> "GET Categorie with :id");
-        put("/category/:id", () -> "PUT Categorie with :id");
-        delete("/category/:id", () -> "DELETE Categorie with :id");
+        get("/category/:id", req -> "GET Categorie with " + req.param("id").intValue());
+        put("/category/:id", req -> "PUT Categorie with " + req.param("id").intValue());
+        delete("/category/:id", req -> "DELETE Categorie with " + req.param("id").intValue());
         /** User **/
         get("/user", () -> "GET User");
         post("/user", () -> "POST User");
         put("/user", () -> "PUT User");
-        delete("/user/:id", () -> "DELETE User with :id");
+        delete("/user/:id", req -> "DELETE User with " + req.param("id").intValue());
     }
 
   public static void main(final String[] args) {
